171539425a2e0f0faa478b437710a0e5ac1edb9c
Jayam Verma <jayam6297@gmail.com>
2024-04-20 19:01:43 +0530

    Added polyfill for call apply and bind

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //? map, filter and reduce 
-//! Using common array for all the polyfill examples
-const arr=[1,2,3,4];
+//! Using common data for all the polyfill examples
+const myArr=[1,2,3,4];
+const car={
+    color: 'Black',
+    company: 'Ferrari'
+}
+function purchasedCar(currency,price) {
+    console.log(`I have purchased ${this.color} ${this.company} car for ${currency}${price}`);
+}
 
 //!-----------------------------------------------------------------------------------------------------------------------------------------
 //* 1) Polyfill for map()
@@ -15,7 +22,7 @@ Array.prototype.customMap=function(cb) {
     return mapTemp;
 }
 //* Example
-const multiplyByTwo=arr.customMap((item) => {
+const multiplyByTwo=myArr.customMap((item) => {
     return item*2;
 })
 // TODO: To see output of above polyfill uncomment below console.log
@@ -34,7 +41,7 @@ Array.prototype.customFilter=function(cb) {
     return filterTemp;
 }
 //? Example
-const moreThenTwo=arr.customFilter((item) => {
+const moreThenTwo=myArr.customFilter((item) => {
     return item > 2;
 })
 // TODO: To see output of above polyfill uncomment below console.log
@@ -54,10 +61,66 @@ Array.prototype.customReduce=function(cb, initialValue) {
     return accumulator;
 }
 //? Example
-const getTotal=arr.customReduce((acc, curr) => {
+const getTotal=myArr.customReduce((acc, curr) => {
     return acc + curr;
 },0)
 // TODO: To see output of above polyfill uncomment below console.log
 //// console.log('getTotal',getTotal);
 
 //!-----------------------------------------------------------------------------------------------------------------------------------------
+
+//? call, bind, and apply in Javascript (Explicit Binding)
+
+//* 4) Polyfill for call() method
+// ? Syntax for reference => call(thisArg, arg1, arg2, /* …, */ argN)
+// ? Polyfill implementation for call()
+Function.prototype.customCall=function(context = {}, ...args) {
+    if(typeof this!=='function') {
+        throw new Error(this+"It's not Callable");
+    }
+
+    context.fn=this;
+    context.fn(...args);
+    return 
+}
+//? Example
+// TODO: To see output of above polyfill uncomment below console.log
+//// purchasedCar.customCall(car, '₹', 10000000)
+
+//!-----------------------------------------------------------------------------------------------------------------------------------------
+//* 5) Polyfill for apply() method
+// ? Syntax for reference => apply(thisArg, argsArray)
+// ? Polyfill implementation for apply()
+Function.prototype.customApply=function(context = {}, args = []) {
+    if(typeof this!=='function') {
+        throw new Error(this+"It's not Callable");
+    }
+    if(!Array.isArray(args)) {
+        throw new TypeError("CreateListFromArrayLike called on non-object");
+    }
+
+    context.fn=this;
+    context.fn(...args);
+}
+//? Example
+// TODO: To see output of above polyfill uncomment below console.log
+//// purchasedCar.customApply(car,['₹',10000000])
+
+//!-----------------------------------------------------------------------------------------------------------------------------------------
+//* 5) Polyfill for bind() method
+// ? Syntax for reference => bind(thisArg, arg1, arg2, /* …, */ argN)
+// ? Polyfill implementation for bind()
+Function.prototype.customBind=function(context = {}, args = []) {
+    if(typeof this!=='function') {
+        throw new Error(this+"cannot be bound as it's not callable");
+    }
+    
+    context.fn=this;
+    return function (...newArgs) {
+        return context.fn(...args,...newArgs);
+    }
+}
+//? Example
+const newFun=purchasedCar.customBind(car,'₹');
+// TODO: To see output of above polyfill uncomment below console.log
+//// console.log(newFun(1000000));
